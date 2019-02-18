c95880b0b97fdcb54b66045a9a5ac58f523f4c27
martinusbe <martinusbe@gmail.com>
2019-02-18 02:44:18 +0100

    fajita: build gzosp

--- gzosp_fajita.mk
+++ gzosp_fajita.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fajita device
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
-# Inherit some common Validus stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+# Inherit some common gzosp stuff.
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := validus_fajita
+PRODUCT_NAME := gzosp_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
